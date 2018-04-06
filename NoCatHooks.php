bfb4abcffeb445a9ac7403b67bfea808dc73f6a9
Brian Wolff <bawolff+wn@gmail.com>
2018-04-06 20:19:16 +0000

    Fix php warning. Forgot to add static modifier.
    d'oh
    
    Change-Id: Ifccd5f2a7ee091bd9fa4a794f7ace51309facf56

--- NoCatHooks.php
+++ NoCatHooks.php
@@ -6,7 +6,7 @@ class NoCatHooks {
 	 *
 	 * @param Array $doubleUnderscoreIDs list of ids
 	 */
-	public function onGetDoubleUnderscoreIDs( array &$doubleUnderscoreIDs ) {
+	public static function onGetDoubleUnderscoreIDs( array &$doubleUnderscoreIDs ) {
 		$doubleUnderscoreIDs[] = 'nocat';
 	}
 
@@ -18,7 +18,7 @@ class NoCatHooks {
 	 * @param string $text The html output
 	 * @param StripState $stripState
 	 */
-	public function onParserAfterParse( Parser $parser, $text, $stripState ) {
+	public static function onParserAfterParse( Parser $parser, $text, $stripState ) {
 		global $wgNoCatShowCat;
 		$pout = $parser->getOutput();
 		if ( $pout->getProperty( 'nocat' ) !== false ) {
@@ -30,7 +30,7 @@ class NoCatHooks {
 		}
 	}
 
-	public function noCatParserOutputHook( OutputPage $out, ParserOutput $parserOutput, $data ) {
+	public static function noCatParserOutputHook( OutputPage $out, ParserOutput $parserOutput, $data ) {
 		if ( $out->getConfig()->get( 'NoCatShowCat' ) && $data ) {
 			$out->addCategoryLinks( $data );
 		}
